90b7e4ab069cf41dc3f448a58f6e3205bc09f755
Aman U <amanahmed2274@gmail.com>
2025-04-16 12:31:06 +0500

    Initial commit

---
+++ qrcode_generate.py
@@ -0,0 +1,21 @@
+
+# 1.  pip install qrcode
+# 2. pip install pillow
+
+import qrcode
+
+def qr_code(data):
+
+ 
+ qr = qrcode.make(data)
+ qr.save("linkedin_profile.png")
+ 
+ print("QR code generated and saved as 'linkedin_profile.png'")
+
+
+
+qr_code("https://www.linkedin.com/in/aman-u-350a8a284/")
+
+
+
+
